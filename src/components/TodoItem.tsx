6b6c209457c26110c0d39fcde134c06b5a0060e6
Samaan Kamal <samaankamal72@gmail.com>
2023-10-18 19:08:01 +0300

    added TodoItem component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoItem:React.FC<{item:{
+    id: string,
+    text:string
+}}> = (props)=>{
+    return (
+        <li key={props.item.id}>{props.item.text}</li>
+    );
+};
+
+export default TodoItem
